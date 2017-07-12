82a536224f78338672da5a85409ddd2fe6ddec32
sahbia <ouaja.sahbia@gmail.com>
2017-07-12 09:58:50 +0200

    add button for slider

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 
 
-
+  //function for slider auto
  var   k=-1;
 
  function play()
 
 {
 	    
-	    var f=document.getElementsByTagName('img');
+	    var f=document.getElementsByTagName('figure');
 	    for( j=0; j<f.length; j++)
 	    {
 	    	f[j].classList.add("hid");
@@ -19,19 +19,22 @@
 	    
 	  if(k<f.length-1){ k++;}
 	  else{k=0;}
-        // if(f[k].classList.contains("hid"))
+        
 	
-         // {
+     
          	   f[k].classList.remove("hid");
          	   f[k].classList.add("visible");
+         	   setTimeout(function(){ f[k].img.classList.add("anim");},500);
          	   f[k].style.border="2px solid black";
          	   
-         // }
+        
        
       
 
 
 }
+
  
 
 setInterval(function(){ play();},2000);
+document.getElementById("myBtn").addEventListener("click", myFunction);
